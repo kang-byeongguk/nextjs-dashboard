2d84ffe69b7e310e494956b8f74d063ed39347d2
byeongkookkang <byeongguk1998@gmail.com>
2025-12-08 16:56:51 +0900

    dashboard/invoices 수정기능, 삭제기능 추가

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,8 +8,10 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
     id: z.string(), customerId: z.string(), amount: z.coerce.number(), status: z.enum(['pending', 'paid']), date: z.string()
 })
-
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+
 export async function createInvoice(formData: FormData) {
     const {customerId,amount,status} = CreateInvoice.parse({ customerId: formData.get('customerId'), amount: formData.get('amount'), status: formData.get('status') });
     const amountInCents=amount*100;
@@ -21,3 +23,24 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+export async function updateInvoice(id:string,formData: FormData) {
+    const {customerId,amount,status} = CreateInvoice.parse({ customerId: formData.get('customerId'), amount: formData.get('amount'), status: formData.get('status') });
+    const amountInCents=amount*100;
+    
+    await sql`
+        UPDATE invoices
+        SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
+        WHERE id=${id}
+    `
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id:string){
+    await sql`
+        DELETE FROM invoices WHERE id =${id}
+    `
+        revalidatePath('/dashboard/invoices');
+
+}
+
